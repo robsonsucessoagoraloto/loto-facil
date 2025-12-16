dde0a64c3036b284fa920f7e8c33d22a373fea22
robsonsucessoagoraloto <robson.sucessoagora@gmail.com>
2025-12-16 20:48:01 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Lotofácil Analyzer")
+
+st.title("📊 Analisador da Lotofácil")
+st.write("Sistema de análise estatística da Lotofácil.")
+st.write("Sem promessas. Apenas dados, estatística e simulações.")
